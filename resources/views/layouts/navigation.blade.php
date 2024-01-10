64f75fe9707d2c70d001c7d38b881d01d2c41d09
karol-jedrzejak <karol.jedrzejak@gmail.com>
2024-01-10 14:05:15 +0100

    up 2024.01.10

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -36,15 +36,17 @@
                 Logout</a>
           </li>
 
-
-{{--           <li class="nav-item dropdown">
+{{--
+    <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
-          </li> --}}
+          </li>
+--}}
+
         </ul>
       </div>
     </div>
