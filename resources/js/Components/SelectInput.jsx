e5065fa3fdfef2784d258b9106aaa8ce95429b91
karol-jedrzejak <karol.jedrzejak@gmail.com>
2025-01-09 14:58:58 +0100

    up

---
+++ resources/js/Components/SelectInput.jsx
@@ -0,0 +1,35 @@
+import { forwardRef, useEffect, useRef } from "react";
+
+export default forwardRef(function SelectInput(
+    { type = "text", className = "", isFocused = false, ...props },
+    ref
+) {
+    const input = ref ? ref : useRef();
+
+    useEffect(() => {
+        if (isFocused) {
+            input.current.focus();
+        }
+    }, []);
+
+    return (
+        <input
+            {...props}
+            type={type}
+            className={
+                "border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm " +
+                className
+            }
+            ref={input}
+        />
+        /*         <select name="pets" id="pet-select">
+            <option value="">--Please choose an option--</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+        </select> */
+    );
+});
